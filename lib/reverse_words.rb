53b7c78ec44ff5654419786f3027c3a86c94cc26
Kate Pond <kate@thepondsedge.com>
2018-03-20 22:43:20 -0700

    creates a method to reverse words in sentence by trying a new technique for reversing charaters in a string

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,28 @@
+require 'pry'
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  raise NotImplementedError
+  return if my_words.nil?
+  return my_words if my_words.length == 0 || my_words.length == 1
+
+  index = 0
+  while index < my_words.length
+    pointer = index
+    while my_words[pointer] != " " && my_words[pointer] != nil
+      pointer += 1
+    end
+
+    if pointer != index
+      start_point = index
+      end_point = pointer - 1
+      while start_point < end_point
+        temp = my_words[start_point]
+        my_words[start_point] = my_words[end_point]
+        my_words[end_point] = temp
+        start_point += 1
+        end_point -= 1
+      end
+    end
+
+    index = pointer + 1
+  end
 end
